5922b1758b02835a9ea5ae49a632b76013c0899c
Alexander Makarov <sam@rmcreative.ru>
2019-04-13 22:36:40 +0000

    Apply fixes from StyleCI

--- src/commands/HelloController.php
+++ src/commands/HelloController.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @link http://www.yiiframework.com/
+ *
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
@@ -16,18 +17,21 @@ use yii\console\ExitCode;
  * This command is provided as an example for you to learn how to create console commands.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
+ *
  * @since 2.0
  */
 class HelloController extends Controller
 {
     /**
      * This command echoes what you have entered as the message.
+     *
      * @param string $message the message to be echoed.
+     *
      * @return int Exit code
      */
     public function actionIndex($message = 'hello world')
     {
-        echo $message . "\n";
+        echo $message."\n";
 
         return ExitCode::OK;
     }
